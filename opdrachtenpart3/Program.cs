37d037314a135ff9b498f8fb9f46c2a997367dd6
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-08 10:14:12 +0100

    opdracht 3-29

--- opdrachtenpart3/Program.cs
+++ opdrachtenpart3/Program.cs
@@ -5,6 +5,33 @@ public class Opdrachtenpart2
 
     public static void Main(string[] args)
     {
+        List<int> ages = new List<int>();
+        Console.WriteLine("geef namen en leeftijden met een komma ertussen(bijvoorbeeld: jan,12) Als je zonder iets in te voeren op enter drukt stopt het programma.");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+
+            string[] parts = input.Split(',');
+            string name = parts[0].Trim();
+            int age = int.Parse(parts[1].Trim());
+            ages.Add(age);
+        }
+
+        int maxAge = ages[0];
+        for (int i = 1; i < ages.Count; i++)
+        {
+            if (ages[i] > maxAge)
+            {
+                maxAge = ages[i];
+            }
+        }
+
+        Console.WriteLine("the leeftijd van het oudste persoon is: " + maxAge);
+
     }
 
 }
