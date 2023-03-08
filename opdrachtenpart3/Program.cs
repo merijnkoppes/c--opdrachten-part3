ed35e1602757c90b67da8c0147376ac06d224d28
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-08 08:59:49 +0100

    first commit

---
+++ opdrachtenpart3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+public class Opdrachtenpart2
+{
+
+    public static void Main(string[] args)
+    {
+    }
+
+}
