7f3da51c6c9fc3f4c232d919747b725051cc5b78
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-08 09:52:12 +0100

    opdracht 3-28

--- opdrachtenpart3/Program.cs
+++ opdrachtenpart3/Program.cs
@@ -4,7 +4,18 @@ public class Opdrachtenpart2
 {
 
     public static void Main(string[] args)
-    {
+    { 
+                while (true)
+        {
+            Console.WriteLine("Geef een zin:");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+            string[] parts = input.Split(' ');
+    Console.WriteLine(parts[parts.Length - 1]);
+        }
     }
 
 }
